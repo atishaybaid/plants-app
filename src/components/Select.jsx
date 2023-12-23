e33c3d2b1f0d0dd1dc28b5037bfb502c3ee42e45
atishaybaid <atishaybaid@gmail.com>
2023-12-23 15:55:29 +0530

    text label component added

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,22 +1,34 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import "./Select.css";
 
-const Select = ({ options = [], onSelect = () => { }, placeholder = "" }) => {
+const Select = ({ options = [], onSelect = () => { }, placeholder = "Select..." }) => {
     const [isOptionListOpen, setOptionListOpen] = useState(false);
+    const [selectedItem, setSelectedItem] = useState({});
     const [inputValue, setInputValue] = useState("");
+    const inputRef = useRef(null);
+
+
+    const isOptionSelected = () => {
+        return Object.keys(selectedItem) ? true : false;
+    }
 
 
     const onClickOption = (selectedOption) => {
-        setInputValue(selectedOption.label);
+        setInputValue("");
+        setSelectedItem(selectedOption);
     }
 
     const renderOptionList = () => {
         return options.map((optionItem) => {
-            return (<div className="option-item" onClick={() => {
+            let selectedClass = "";
+            if (isOptionSelected()) {
+                selectedClass = optionItem.label == selectedItem.label ? "selected-item" : "";
+            }
+            return (<div className={"option-item " + selectedClass} onClick={() => {
                 onClickOption(optionItem)
             }}>
                 {optionItem.label}
-            </div>)
+            </div >)
         })
     }
 
@@ -26,9 +38,39 @@ const Select = ({ options = [], onSelect = () => { }, placeholder = "" }) => {
         setOptionListOpen(true);
     }
 
+    const handleInputBlur = () => {
+        // if (isOptionSelected()) {
+        //     setInputValue(selectedItem.label);
+        // } else {
+        //     setInputValue("");
+        // }
+        setInputValue("");
+
+    }
+
+    const getInputValueToDisaply = () => {
+        if (inputValue) {
+            return inputValue;
+        }
+
+        if (isOptionSelected()) {
+            return selectedItem.label;
+        }
+        return ""
+    }
+    const onClickSelectedLabelBox = () => {
+        inputRef.current.focus();
+    }
+
     return (
         <div className="select-container">
-            <input type="search" onChange={onChangeInputBox} onClick={onChangeInputBox} value={inputValue} placeholder={inputValue ? inputValue : placeholder}></input>
+            <input type="search" className="select-input" onChange={onChangeInputBox}
+                onClick={onChangeInputBox} value={inputValue}
+                placeholder={isOptionSelected ? selectedItem.label : placeholder}
+                onBlur={handleInputBlur} ref={inputRef} >
+
+            </input>
+            <span className="selected-label" onClick={onClickSelectedLabelBox}>{isOptionSelected ? selectedItem.label : ""}</span>
             {isOptionListOpen ? <div className="option-list" >
                 {renderOptionList()}
             </div> : null}
@@ -37,6 +79,19 @@ const Select = ({ options = [], onSelect = () => { }, placeholder = "" }) => {
     )
 
 }
+
+
+/*
+Logic for input box behaviour
+- when user types, value should be filled
+- when user selects something from option list,input box should have that value only
+- when user types something and some value is also selected,then once typing is over(on blur of input)the selected value should be replaced
+
+Next 
+fix select behaviour with span
+
+
+*/
 
 
 
--- src/components/Select.md
+++ src/components/Select.md
@@ -1,5 +1,5 @@
 #### todo-21/12
 - Make a basic css
--  By default list should display 10 items,and should have scroll
+- By default list should display 10 items,and should have scroll
 - When an option is selected then it should darken up[some kind of highlight]
 - The list should open up when select is clicked by the user and should close when clicked outside or on the list to select the value.
