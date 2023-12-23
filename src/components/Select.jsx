5c9e5b9db5cb212bbeabb054f74e6e2861790a6e
atishaybaid <atishaybaid@gmail.com>
2023-12-23 15:55:29 +0530

    select behaviour fixes on click

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react"
+import { useState, useRef, useEffect } from "react"
 import "./Select.css";
 
 const Select = ({ options = [], onSelect = () => { }, placeholder = "Select..." }) => {
@@ -6,6 +6,7 @@ const Select = ({ options = [], onSelect = () => { }, placeholder = "Select..." 
     const [selectedItem, setSelectedItem] = useState({});
     const [inputValue, setInputValue] = useState("");
     const inputRef = useRef(null);
+    const labelRef = useRef(null);
 
 
     const isOptionSelected = () => {
@@ -18,13 +19,44 @@ const Select = ({ options = [], onSelect = () => { }, placeholder = "Select..." 
         setSelectedItem(selectedOption);
     }
 
+    useEffect(() => {
+        const elms = [inputRef.current, labelRef.current]
+        window.addEventListener("click", (event) => {
+            console.log(event.target);
+            // const condition = elms.every((elm) => {
+            //     event.target != elm;
+            // });
+
+            // console.log(condition);
+            // if (condition) {
+            //     setOptionListOpen(false);
+            // }
+
+            if (elms[0] != event.target && elms[1] != event.target) {
+                setOptionListOpen(false);
+            }
+
+            // if (!elms.every(event.target)) {
+
+            //     console.log("on click also called");
+            // }
+
+            // if (inputRef.current && !inputRef.current.contains(event.target)) {
+
+            // }
+
+        })
+
+    }, [])
+
     const renderOptionList = () => {
         return options.map((optionItem) => {
             let selectedClass = "";
             if (isOptionSelected()) {
                 selectedClass = optionItem.label == selectedItem.label ? "selected-item" : "";
             }
-            return (<div className={"option-item " + selectedClass} onClick={() => {
+            return (<div className={"option-item " + selectedClass} onClick={(event) => {
+                // event.stopPropagation();
                 onClickOption(optionItem)
             }}>
                 {optionItem.label}
@@ -32,8 +64,10 @@ const Select = ({ options = [], onSelect = () => { }, placeholder = "Select..." 
         })
     }
 
-    const onChangeInputBox = ({ target }) => {
+    const onChangeInputBox = (event) => {
+        const { target } = event
         setInputValue(target.value);
+        event.stopPropagation();
 
         setOptionListOpen(true);
     }
@@ -61,16 +95,32 @@ const Select = ({ options = [], onSelect = () => { }, placeholder = "Select..." 
     const onClickSelectedLabelBox = () => {
         inputRef.current.focus();
     }
+    const isLabelVisible = () => {
+        if (inputValue || !isOptionSelected()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    const listEventOnFocus = (event) => {
+        console.log("listEventOnFocus called");
+        setOptionListOpen(true);
+        event.stopPropagation();
+    }
+
+
 
     return (
         <div className="select-container">
             <input type="search" className="select-input" onChange={onChangeInputBox}
                 onClick={onChangeInputBox} value={inputValue}
-                placeholder={isOptionSelected ? selectedItem.label : placeholder}
-                onBlur={handleInputBlur} ref={inputRef} >
+                placeholder={placeholder}
+                onBlur={handleInputBlur} ref={inputRef} onFocus={listEventOnFocus} >
 
             </input>
-            <span className="selected-label" onClick={onClickSelectedLabelBox}>{isOptionSelected ? selectedItem.label : ""}</span>
+            {isLabelVisible() ?
+                <span className="selected-label" onClick={onClickSelectedLabelBox} ref={labelRef}>{isOptionSelected ? selectedItem.label : ""}</span> : null}
             {isOptionListOpen ? <div className="option-list" >
                 {renderOptionList()}
             </div> : null}
@@ -89,7 +139,8 @@ Logic for input box behaviour
 
 Next 
 fix select behaviour with span
-
+hide the label when input value is changed
+    - stop propogation of the on click event
 
 */
 
