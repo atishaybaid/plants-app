f702e7590c915d313a539496a5db15fc8c859955
atishaybaid <atishaybaid@gmail.com>
2023-12-23 15:55:28 +0530

    base setup done

--- src/PlantList.jsx
+++ src/PlantList.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { fetchData } from "./Services/fetchData";
 import PlantCard from "./PlantCard";
+import Modal from 'react-modal';
 
 const PlantList = () => {
   const [plantListData, setPlantListData] = useState([]);
-  const [plantListIsLoading,setPlantListIsLoading] = useState(false);
+  const [plantListIsLoading, setPlantListIsLoading] = useState(false);
+  const [isDetailsModalOpen, setIsDetailModalOpen] = useState(false);
+
   useEffect(() => {
     setPlantListIsLoading(true)
     fetchData(
@@ -20,18 +23,45 @@ const PlantList = () => {
         setPlantListIsLoading(false)
       });
   }, []);
+
+  const closeModal = () => {
+    setIsDetailModalOpen(false);
+  }
+
+  const renderDetailsModal = () => {
+    return (
+      <Modal
+        isOpen={isDetailsModalOpen}
+        contentLabel="Example Modal"
+        onRequestClose={closeModal}
+      >
+
+        <div>I am a modal</div>
+
+      </Modal>
+    )
+  }
+  const openModal = () => {
+    setIsDetailModalOpen(true);
+  }
+
+
+
   const renderPlantList = () => {
     return (
-      <ul>
+      <ul onClick={openModal}>
         {plantListData.map((plant) => (
           <PlantCard plant={plant} key={plant.id} />
-        
+
         ))}
       </ul>
     );
   };
 
-  return <div>{renderPlantList()}</div>;
+  return <div>{renderPlantList()}
+    {renderDetailsModal()}
+
+  </div>;
 };
 
 export default PlantList;
